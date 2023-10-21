9cb94e8a1ac2408d2c0152d435a5a1b22d8978c9
EduardoMaltauro <maltauroeduardo717@gmail.com>
2023-10-21 12:04:32 -0300

    Electron

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const {app, BrowserWindow} = require("electron")
+
+app.whenReady()
+    .then(function() {
+        const janela = new BrowserWindow({
+            autoHideMenuBar: true,
+            height: 600,
+            resizable: false,
+            width: 600
+        })
+
+        janela.loadFile("./public/index.html")
+    })
